52e28b79304211a1ff6854373a1208bee9d4c396
Serwur <lychaserwur@gmail.com>
2018-12-08 04:48:18 +0100

    Player Movement Velocity

--- Sensei2k18/Assets/Scripts/PlayerMovement.cs
+++ Sensei2k18/Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
 
 [RequireComponent( typeof( PlayerInput ) )]
+[RequireComponent( typeof( Rigidbody2D ) )]
 public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 10f;
-    public PlayerInput input;
+
+    private PlayerInput input;
+    private Rigidbody2D rigidBody;
 
     public void Awake()
     {
         input = GetComponent<PlayerInput>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -19,15 +23,17 @@ public class PlayerMovement : MonoBehaviour
 
     public void Movement()
     {
-        float x = input.GetXAxis();
-        float y = input.GetYAxis();
+        float x = input.GetXAxis() * Time.deltaTime * moveSpeed;
+        float y = input.GetYAxis() * Time.deltaTime * moveSpeed;
+
+        rigidBody.velocity.Set( x, y );
 
         Vector3 moveDirection = new Vector3( x, y, 0 ) * Time.deltaTime * moveSpeed;
         transform.position = transform.position + moveDirection;
 
         if (moveDirection.magnitude != 0) {
-        //   Quaternion rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation( moveDirection, transform.forward ), 0.8f );
-           // transform.rotation.Set( rotation.x);
+            //   Quaternion rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation( moveDirection, transform.forward ), 0.8f );
+            // transform.rotation.Set( rotation.x);
         }
 
     }
