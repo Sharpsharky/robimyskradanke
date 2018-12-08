85b1814f670ce1be9cbd3b19cc0019f241aba212
Sharpshark <maciej.jasielewicz@gmail.com>
2018-12-08 02:37:50 +0100

    Prosta animacja

---
+++ Sensei2k18/Assets/gateAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gateAnimator : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKey("space"))
+        {
+            Debug.Log("OPEN THE GATE");
+            anim.SetBool("cienStoiNaPikawie", true);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            Debug.Log("CLOSE THE GATE");
+            anim.SetBool("cienStoiNaPikawie", false);
+        }
+    }
+
+}
