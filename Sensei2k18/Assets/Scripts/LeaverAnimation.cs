0c1df643eba0d77fa8139f08d4278e8a5fe0cb36
Sharpshark <maciej.jasielewicz@gmail.com>
2018-12-08 16:38:58 +0100

    Wajcha

---
+++ Sensei2k18/Assets/Scripts/LeaverAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaverAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Wajcha open");
+            anim.SetBool("Pociagnieta", true);
+        }
+        if (Input.GetKeyUp("space"))
+        {
+            Debug.Log("Wajcha close");
+            anim.SetBool("Pociagnieta", false);
+        }
+    }
+}
