e821648c7bce1dee5e743bd902b9b9652331dacc
Sharpshark <maciej.jasielewicz@gmail.com>
2018-12-09 13:17:30 +0100

    bangla wszystko bez muzy

---
+++ Sensei2k18/Assets/Scripts/DESK.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DESK : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Human")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
