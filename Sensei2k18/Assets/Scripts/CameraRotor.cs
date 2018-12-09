5e26348f9722afcb5b970fc32b440bbe8792ff98
Serwur <lychaserwur@gmail.com>
2018-12-09 03:30:49 +0100

    skrypty kamery

---
+++ Sensei2k18/Assets/Scripts/CameraRotor.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+
+public class CameraRotor : MonoBehaviour
+{
+
+    public enum Direction
+    {
+        Left, Right
+    }
+
+    public float startAngle = 0f;
+    public float angleRotation = 180f;
+    public float rotateSpeed = 0.2f;
+
+    private Quaternion startDestination;
+    private Quaternion endDestination;
+    private Quaternion currentDestination;
+    private bool isEndNext = false;
+
+    public void Start()
+    {
+        transform.rotation = Quaternion.Euler( 0f, startAngle, 0f );
+        startDestination = transform.rotation;
+        endDestination = Quaternion.Euler( 0f, startAngle + angleRotation, 0f );
+        currentDestination = endDestination;
+    }
+
+    public void Update()
+    {
+        transform.rotation = Quaternion.Slerp( transform.rotation, currentDestination, rotateSpeed );
+        if ( transform.rotation == currentDestination ) {
+            NextDestination();
+        }
+      
+    }
+
+    public void NextDestination()
+    {
+        if ( isEndNext ) {
+            currentDestination = endDestination;
+            isEndNext = !isEndNext;
+        } else {
+
+        }
+    }
+}
---
+++ Sensei2k18/Assets/Scripts/CameraTriggered.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTriggered : Triggered {
+
+    private Transform mesh;
+    private Transform cameraLight;
+
+    public new void Awake()
+    {
+        base.Awake();
+        mesh = transform.Find("CameraMesh");
+        cameraLight = transform.Find("MigajaSzmata");
+    }
+
+    public override void OnActive()
+    {
+        base.OnActive();
+        mesh.gameObject.SetActive(true);
+        cameraLight.gameObject.SetActive( true );
+    }
+
+    public override void OnDeactive()
+    {
+        base.OnDeactive();
+        mesh.gameObject.SetActive( false );
+        cameraLight.gameObject.SetActive( false );
+    }
+
+}
